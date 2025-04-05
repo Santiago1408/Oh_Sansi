dd65fb15680bbda13b1ab3d8d52c1a2e6ce73316
Josue <josueg4rcia@gmail.com>
2025-04-05 13:42:11 -0400

    Formulario competidor sin estilos (/registro)

---
+++ resources/views/form-competidor.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('css/form-competidor.css') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscribirse</title>
+</head>
+<body>
+    <header>
+        <h2>Oh! Sansi</h2>
+    </header>
+    <div class="formulario">
+        <h2 id="titFormulario">FORMULARIO DE INSCRIPCIÓN</h2>
+        <h2>COMPETIDOR</h2>
+        <div class="campos">
+            <div class="seccion1">
+                <p>Nombres: </p>
+                <input type="text" name="nombres" id="">
+                <p>Apellidos: </p>
+                <input type="text" name="apellidos" id="">
+                <p>Fecha de nacimeinto: </p>
+                <input type="date" name="fechaNacimiento" id="">
+                <p>Carnet de identidad:</p>
+                <input type="text" name="ci" id="">
+                <p>Correo electrónico: </p>
+                <input type="email" name="email" id="">
+            </div>
+            <div class="seccion2">
+                <p>Número de celular: </p>
+                <input type="text" name="numCelular" id="">
+                <p>Colegio: </p>
+                <input type="text" name="colegio" id="">
+                <p>Curso: </p>
+                <select name="curso" id="">
+                    <option value="1">1ro Secundaria</option>
+                    <option value="2">2do Secundaria</option>
+                    <option value="3">3ro Secundaria</option>
+                    <option value="4">4to Secundaria</option>
+                    <option value="5">5to Secundaria</option>
+                    <option value="6">6to Secundaria</option>
+                </select>
+                <p>Provincia: </p>
+                <input type="text" name="provincia" id="">
+                <p>Departamento: </p>
+                <input type="text" name="departamento" id="">
+                <div class="botones">
+                    <input type="reset" value="Cancelar">
+                    <input type="submit" value="Siguiente">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
