a7ff7108cf3b8e54b2e3a5204f65f3be9ceb763f
Josue <josueg4rcia@gmail.com>
2025-04-05 13:26:54 -0400

    Pagina de inicio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <title>Oh! Sansi</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="img/Marca_Vertical_Universidad_Mayor_de_San_Simón_Cochabamba_Bolivia.png" alt="">
+            <h1 class="oh-sansi">Oh! Sansi</h1>
+        </div>
+        <nav>
+            <a href="" class="nav-link">Administrador</a>
+            <a href="" class="nav-link">Cajero</a>
+            <a href="" class="nav-link">Tutor</a>
+            <a href="" class="nav-link" id="inscripcion">Inscripción</a>
+        </nav>
+    </header>
+    <main>
+        <div class="bienvenida">
+            <div class="info">
+                <h2>Olimpiadas en Ciencias y Tecnología San Simón</h2>
+                <h3>La facultad de Ciencias y Tecnología de la Universidad Mayor de San Simón convoca a los estudiantes
+                    del Sistema de Educación Regular a participar en las olimpiadas Oh! Sansi
+                </h3>
+                <p>
+                    Matemáticas, Física, Química, Biología, Astronomía y Astrofísica, Informática, Robótica
+                </p>
+                <input type="button" id="insc" value="Inscribirse">
+            </div>
+            <div class="mascota">
+                <img src="img/robot-sansi.png" alt="" srcset="">
+            </div>
+        </div>
+    </main>
+</body>
+</html>
